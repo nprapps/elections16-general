6c57eb20559eeffa201bb81bb4f3b48fd5724c88
Tyler Fisher <tfisher@npr.org>
2016-10-07 16:56:53 -0400

    debug statements

--- fabfile/data.py
+++ fabfile/data.py
@@ -40,13 +40,17 @@ def create_db():
             servers.stop_service('deploy')
 
         with shell_env(**app_config.database):
+            print('dropping db')
             local('dropdb --if-exists %s' % app_config.database['PGDATABASE'])
 
         if not env.get('settings'):
+            print('dropping user')
             local('psql -c "DROP USER IF EXISTS %s;"' % app_config.database['PGUSER'])
+            print('creating user')
             local('psql -c "CREATE USER %s WITH SUPERUSER PASSWORD \'%s\';"' % (app_config.database['PGUSER'], app_config.database['PGPASSWORD']))
 
         with shell_env(**app_config.database):
+            print('creating db')
             local('createdb %s' % app_config.database['PGDATABASE'])
 
         if env.get('settings'):
@@ -55,6 +59,7 @@ def create_db():
 
 @task
 def create_tables():
+    print('creating tables')
     models.Result.create_table()
     models.Call.create_table()
     models.RaceMeta.create_table()
