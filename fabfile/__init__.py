2e0c8c0f4518a873412e3b0d84adec002dcacba5
Tyler Fisher <tfisher@npr.org>
2016-10-12 17:26:29 -0400

    major repo cleanup

--- fabfile/__init__.py
+++ fabfile/__init__.py
@@ -12,21 +12,14 @@ from termcolor import colored
 import app_config
 
 # Other fabfiles
-from . import assets
 from . import daemons
 from . import data
-from . import flat
 from . import issues
 from . import render
-from . import text
 from . import utils
-from . import spreadsheet
 
 if app_config.DEPLOY_TO_SERVERS:
     from . import servers
-
-if app_config.DEPLOY_CRONTAB:
-    from . import cron_jobs
 
 # Bootstrap can only be run once, then it's disabled
 if app_config.PROJECT_SLUG == '$NEW_PROJECT_SLUG':
@@ -105,16 +98,6 @@ def app(port='8000'):
         local('gunicorn -b 0.0.0.0:%s --timeout 3600 --debug --reload --log-file=logs/app.log app:wsgi_app' % port)
 
 @task
-def public_app(port='8001'):
-    """
-    Serve public_app.py.
-    """
-    if env.get('settings'):
-        local("DEPLOYMENT_TARGET=%s bash -c 'gunicorn -b 0.0.0.0:%s --timeout 3600 --debug --reload --log-file=logs/public_app.log public_app:wsgi_app'" % (env.settings, port))
-    else:
-        local('gunicorn -b 0.0.0.0:%s --timeout 3600 --debug --reload --log-file=logs/public_app.log public_app:wsgi_app' % port)
-
-@task
 def tests():
     """
     Run Python unit tests.
@@ -128,130 +111,34 @@ Changes to deployment requires a full-stack test. Deployment
 has two primary functions: Pushing flat files to S3 and deploying
 code to a remote server if required.
 """
-@task
-def update():
-    """
-    Update all application data not in repository (copy, assets, etc).
-    """
-    utils.install_font(force=False)
-    text.update()
-    assets.sync()
-    data.update()
-
-@task
-def deploy(remote='origin', reload=False):
-    """
-    Deploy the latest app to S3 and, if configured, to our servers.
-    """
-    require('settings', provided_by=[production, staging])
-
-    if app_config.DEPLOY_TO_SERVERS:
-        require('branch', provided_by=[stable, master, branch])
-
-        if (app_config.DEPLOYMENT_TARGET == 'production' and env.branch != 'stable'):
-            utils.confirm(
-                colored("You are trying to deploy the '%s' branch to production.\nYou should really only deploy a stable branch.\nDo you know what you're doing?" % env.branch, "red")
-            )
-
-        servers.checkout_latest(remote)
-
-        servers.fabcast('text.update')
-        servers.fabcast('assets.sync')
-        servers.fabcast('data.update')
-
-        if app_config.DEPLOY_CRONTAB:
-            servers.install_crontab()
-
-        if app_config.DEPLOY_SERVICES:
-            servers.deploy_confs()
-
-    update()
-    render.render_all()
-
-    # Clear files that should never be deployed
-    local('rm -rf www/live-data')
-
-    flat.deploy_folder(
-        app_config.S3_BUCKET,
-        'www',
-        app_config.PROJECT_SLUG,
-        headers={
-            'Cache-Control': 'max-age=%i' % app_config.DEFAULT_MAX_AGE
-        },
-        ignore=['www/assets/*', 'www/live-data/*']
-    )
-
-    flat.deploy_folder(
-        app_config.S3_BUCKET,
-        'www/assets',
-        '%s/assets' % app_config.PROJECT_SLUG,
-        headers={
-            'Cache-Control': 'max-age=%i' % app_config.ASSETS_MAX_AGE
-        }
-    )
-
-    if reload:
-        reset_browsers()
-
-    if not check_timestamp():
-        reset_browsers()
 
 @task
 def deploy_national_data():
     local('rm -rf {0}'.format(app_config.DATA_OUTPUT_FOLDER))
     local('mkdir {0}'.format(app_config.DATA_OUTPUT_FOLDER))
 
-    data.render_all_national()
-
-    local('aws s3 sync {0} s3://{1}/{2}/data/ --acl public-read'.format(app_config.DATA_OUTPUT_FOLDER, app_config.S3_BUCKET, app_config.PROJECT_SLUG))
-
+    render.render_all_national()
+    deploy_data_folder()
 
 @task
 def deploy_presidential_data():
     local('rm -rf {0}'.format(app_config.DATA_OUTPUT_FOLDER))
     local('mkdir {0}'.format(app_config.DATA_OUTPUT_FOLDER))
 
-    data.render_presidential_files()
+    render.render_presidential_files()
+    deploy_data_folder()
 
+@task
+def deploy_all_data():
+    local('rm -rf {0}'.format(app_config.DATA_OUTPUT_FOLDER))
+    local('mkdir {0}'.format(app_config.DATA_OUTPUT_FOLDER))
+
+    render.render_all()
+    deploy_data_folder()
+
+@task
+def deploy_data_folder():
     local('aws s3 sync {0} s3://{1}/{2}/data/ --acl public-read'.format(app_config.DATA_OUTPUT_FOLDER, app_config.S3_BUCKET, app_config.PROJECT_SLUG))
-
-@task
-def check_timestamp():
-    require('settings', provided_by=[production, staging])
-
-    bucket = utils.get_bucket(app_config.S3_BUCKET)
-    k = Key(bucket)
-    k.key = '%s/live-data/timestamp.json' % app_config.PROJECT_SLUG
-    if k.exists():
-        return True
-    else:
-        return False
-
-@task
-def reset_browsers():
-    """
-    Deploy a timestamp so the client will reset their page. For bugfixes
-    """
-    require('settings', provided_by=[production, staging])
-
-    if not os.path.exists('www/live-data'):
-        os.makedirs('www/live-data')
-
-    payload = {}
-    now = datetime.now().strftime('%s')
-    payload['timestamp'] = now
-
-    with open('www/live-data/timestamp.json', 'w') as f:
-        json.dump(payload, f)
-
-    flat.deploy_folder(
-        app_config.S3_BUCKET,
-        'www/live-data',
-        '%s/live-data' % app_config.PROJECT_SLUG,
-        headers={
-            'Cache-Control': 'max-age=%i' % app_config.DEFAULT_MAX_AGE
-        }
-    )
 
 """
 Destruction
